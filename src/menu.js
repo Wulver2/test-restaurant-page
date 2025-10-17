f0bab768bd5a9725dd45b2e2eb25cb0ca801cddc
Wulver2 <ross.shayla21@gmail.com>
2025-10-17 11:28:12 -0400

    Added two example menu items and displayed them using grid

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,25 @@
+import cacio from "./imgs/Cacio_e_Pepe.jpg"
+import pizza from "./imgs/Margherita_Pizza.jpg"
+
 export function menuInfo() {
     var menu = document.createElement("div");
     var heading = document.createElement("h1")
+    var img1 = document.createElement("img");
+    var img2 = document.createElement("img");
+    var c = document.createElement("p");
+    var m = document.createElement("p");
+
+    menu.id = "menu"
+    img1.src = cacio;
+    c.textContent = "Cacio e Pepe";
+    img2.src = pizza;
+    m.textContent = "Margherita Pizza";
     heading.textContent = "Menu";
-    // append everything to menu and return it
-    menu.appendChild(heading);
-    return menu;
+   
+    menu.appendChild(img1);
+    menu.appendChild(img2);
+    menu.appendChild(c);
+    menu.appendChild(m);
+    heading.append(menu);
+    return heading;
 };
